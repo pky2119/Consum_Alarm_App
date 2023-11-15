c08cec6811b20e8560b482f1bb0fbd0cd5ca4236
pky <pky2119@naver.com>
2023-11-15 19:58:11 +0900

    파이어베이스에 데이터 저장하고 저장한 정보 메인 페이지에서 소비기한에 맞게 설정

--- app/src/main/java/com/example/consum_alarm_app/MainCalendar.kt
+++ app/src/main/java/com/example/consum_alarm_app/MainCalendar.kt
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CalendarView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.firebase.database.*
 
 class MainCalendar : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
+    private lateinit var dateInfo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,9 +20,50 @@ class MainCalendar : AppCompatActivity() {
         val btnRegistProduct = findViewById<Button>(R.id.regist_product)
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
 
+        // Firebase 인스턴스 초기화
+        database = FirebaseDatabase.getInstance()
+
+
+
         btnRegistProduct.setOnClickListener {
             val intent = Intent(this, Regist::class.java)
             startActivity(intent)
         }
+
+        calendarView.setOnDateChangeListener { _, year, month, day ->
+            val selectedDate = String.format("%d-%02d-%02d", year, month + 1, day)
+            loadProductInfo(selectedDate)
+        }
+    }
+
+    private fun loadProductInfo(selectedDate: String) {
+        val myRef = database.getReference("products")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val productInfoLayout = findViewById<LinearLayout>(R.id.productInfoLayout)
+                productInfoLayout.removeAllViews()
+                for (productSnapshot in dataSnapshot.children) {
+                    val product = productSnapshot.getValue(Product::class.java)
+                    if (product != null && product.expirationDate == selectedDate) {
+                        val productInfo = TextView(this@MainCalendar)
+                        productInfo.text = "상품명: ${product.productName}, 가격: ${product.price}, 구매일: ${product.purchaseDate}, 소비기한: ${product.expirationDate}"
+                        productInfoLayout.addView(productInfo)
+                    }
+                }
+            }
+
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Failed to read value
+                // Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 }
+
+data class Product(
+    var productName: String? = null,
+    var purchaseDate: String? = null,
+    var price: String? = null,
+    var expirationDate: String? = null
+)
